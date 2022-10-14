1dfd098675fe9873ad49ad760addb26bd8b91823
sder12 <sara.cetto@gmail.com>
2022-10-14 16:01:25 +0200

    Palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,3 +2,37 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma.
 */
+
+//chiedere all'utende una parola
+//creare funzione isPalindroma
+//check con parola utente
+
+//INPUT UTENTE
+const userWord = prompt("Scrivi una parola");
+console.log(userWord);
+
+const reverseWord = userWord.split('').reverse('').join('')
+console.log(reverseWord)
+
+const userPalindroma = isWordPalindroma(userWord);
+console.log(userPalindroma)
+
+
+
+//FUNCTION
+/**
+ * Description Controllare se parola è palindroma = se il suo riflesso è uguale
+ * @param {string} word parola scelta 
+ * trasformo la parola in riflesso e verifico se sono uguali 
+ * @returns {any}
+ */
+function isWordPalindroma(word){
+    const reverseWord = word.split('').reverse('').join('');
+    if (word === reverseWord){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
